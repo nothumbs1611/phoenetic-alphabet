a4392c6885a860c1150d6bff1b8f0ec4918a6e11
nothumbs1611 <139991106+nothumbs1611@users.noreply.github.com>
2024-07-10 18:22:22 -0400

    Update main.py
    added error checking in case user inputs numbers, or other characters not in the dictionary

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 
-
+import pandas
+#TODO 1. Create a dictionary in this format:{"A": "Alfa", "B": "Bravo"}
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 #dict = {key:value for item in list}
 dict = {row.letter:row.code for (index, row) in data.iterrows()}
 #print(dict)
 
-name = input("Enter the word to convert: ").upper()
-result = [dict[letters] for letters in name]
-print(result)
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+def generate_phoenetic():
+    word = input("Enter the word to convert: ").upper()
+    try:
+        result = [dict[letters] for letters in word]
+    except KeyError:
+        print("only english letters allowed")
+        generate_phoenetic()
+    else:
+        print(result)
+generate_phoenetic()
